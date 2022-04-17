1d6ee473e19dc413686c200dbec9f49f366ac71a
Adrian Chu <18anhc@queensu.ca>
2022-04-17 19:24:12 -0400

    LEVEL 1: fragment collection
    fragments can now be collected (they get destroyed when the player touches them)

---
+++ Assets/Scripts/destroy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
